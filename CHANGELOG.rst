ec5c651c9b8525fb7b86feb708bc05d42de6c21f
Daniel Stonier <d.stonier@gmail.com>
2019-06-22 11:17:51 -0400

    1.0.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -5,3 +5,7 @@ Changelog
 Forthcoming
 -----------
 * ...
+
+1.0.0 (2019-06-22)
+------------------
+* [tutorials] all tutorials except for bagging upgraded for ROS2 dashing
--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'install': OverrideInstall
     },
     name=package_name,
-    version='0.1.0',  # also update package.xml and version.py
+    version='1.0.0',  # also update package.xml and version.py
     packages=find_packages(exclude=['tests*', 'docs*', 'launch*']),
     data_files=[('share/' + package_name, ['package.xml'])],
     package_data={'py_trees_ros_tutorials': ['mock/gui/*']},
