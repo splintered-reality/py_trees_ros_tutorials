56149f4e0f2d639dca12b0cdb36a9e4e626b8c4d
Daniel Stonier <d.stonier@gmail.com>
2019-11-04 17:08:49 -0500

    [mock] emulate_tty for mock robot nodes

--- py_trees_ros_tutorials/mock/launch.py
+++ py_trees_ros_tutorials/mock/launch.py
@@ -41,20 +41,25 @@ def generate_launch_description() -> launch.LaunchDescription:
                       'led_strip', 'move_base', 'rotation_controller',
                       'safety_sensors']:
         node_executable = "mock-{}".format(node_name.replace('_', '-'))
-        # If using PYTHONUNBUFFERED, but kill the spammy introspection in the
-        # utilities module. Note, PYTHONUNBUFFERED loses colour since it
-        # doesn't emulate a tty terminal
-        # myenv = os.environ
-        # myenv["PYTHONUNBUFFERED"] = "1"
-        launch_description.add_action(
-            launch_ros.actions.Node(
-                package='py_trees_ros_tutorials',
-                node_name=node_name,
-                node_executable=node_executable,
-                output='screen',
-                # env=myenv
+        try:
+            launch_description.add_action(
+                launch_ros.actions.Node(
+                    package='py_trees_ros_tutorials',
+                    node_name=node_name,
+                    node_executable=node_executable,
+                    output='screen',
+                    emulate_tty=True
+                )
             )
-        )
+        except TypeError:
+            launch_description.add_action(
+                launch_ros.actions.Node(
+                    package='py_trees_ros_tutorials',
+                    node_name=node_name,
+                    node_executable=node_executable,
+                    output='screen',
+                )
+            )
     return launch_description
 
 ##############################################################################
