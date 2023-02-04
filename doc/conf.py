3c56e7609820cfb39babadb9ca21bab38f5a2cc6
Daniel Stonier <d.stonier@gmail.com>
2023-02-03 23:32:59 -0500

    [docs] update intersphinx releases to latest py_trees releases

--- doc/conf.py
+++ doc/conf.py
@@ -62,9 +62,9 @@ exclude_patterns = ['.build', 'weblinks.rst']
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
-    'py_trees': ('https://py-trees.readthedocs.io/en/release-2.0.x', None),
-    'py_trees_ros': ('https://py-trees-ros.readthedocs.io/en/release-2.0.x', None),
-    'rclpy': ('http://docs.ros2.org/eloquent/api/rclpy/', None),
+    'py_trees': ('https://py-trees.readthedocs.io/en/release-2.2.x', None),
+    'py_trees_ros': ('https://py-trees-ros.readthedocs.io/en/release-2.1.x', None),
+    'rclpy': ('http://docs.ros2.org/foxy/api/rclpy/', None),
 }
 
 # -- Options for HTML output ----------------------------------------------
