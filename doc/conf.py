2e7d7b95ed14d1c9dcbc84d5e58fe79c73f3434f
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2025-11-13 19:33:00 -0500

    [docs] Remove rclpy intersphinx mappings (#57)

--- doc/conf.py
+++ doc/conf.py
@@ -64,7 +64,6 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'py_trees': ('https://py-trees.readthedocs.io/en/release-2.4.x', None),
     'py_trees_ros': ('https://py-trees-ros.readthedocs.io/en/release-2.4.x', None),
-    'rclpy': ('http://docs.ros2.org/jazzy/p/rclpy/', None),
 }
 
 # -- Options for HTML output ----------------------------------------------
